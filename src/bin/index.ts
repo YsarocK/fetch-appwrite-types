f622b0eb06be61078d2ace46ab7c467e1aa1bbf5
Etienne Moureton <etienne.moureton@unflux.fr>
2024-03-01 22:20:38 +0100

    working npx params

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -3,5 +3,21 @@ import { argv } from "node:process";
 import { FetchNewTypes } from "../main.js";
 import type { FetchParameters } from "../types/index.js";
 
-const args = argv.slice(2) as FetchParameters;
-await FetchNewTypes(args);
+const args = argv.slice(2);
+
+const opts: FetchParameters = {}
+
+if(args.includes("hardTypes")) {
+  opts.hardTypes = true;
+}
+
+if(args.includes("includeDBName")) {
+  opts.includeDBName = true;
+}
+
+if(args.includes("outDir")) {
+  const outDirIndex = args.indexOf("outDir");
+  opts.outDir = args[outDirIndex + 1];
+}
+
+await FetchNewTypes(opts);
