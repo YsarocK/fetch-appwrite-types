e6218d3949509dc0d6db25cf4f5aac133673d3cd
Etienne Moureton <etienne.moureton@unflux.fr>
2024-03-02 19:01:29 +0100

    date handled as string in light

--- src/utils/FindType.ts
+++ src/utils/FindType.ts
@@ -17,9 +17,13 @@ const FindType = (attribute: Attribute, outDir: string, intfName: string, hardTy
     return create.property(attribute.key, type.null, attribute.required === false && DeclarationFlags.Optional);
   }
 
-  // handle null values
+  // handle date values
   if (attribute.type === "datetime") {
-    return create.property(attribute.key, create.namedTypeReference('Date'), attribute.required === false && DeclarationFlags.Optional);
+    if(hardTypes) {
+      return create.property(attribute.key, create.namedTypeReference('Date'), attribute.required === false && DeclarationFlags.Optional);
+    }
+
+    return create.property(attribute.key, type.string, attribute.required === false && DeclarationFlags.Optional);
   }
 
   // handle related collections
